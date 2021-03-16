104f5218a23e2b4f3a30c3faf5297181a0f791e2
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-03-16 17:21:08 -0500

    Update update.py

--- update.py
+++ update.py
@@ -67,13 +67,16 @@ def print_repository(repo):
         print('Remote named "{}" with URL "{}"'.format(remote, remote.url))
     print('Last commit for repo is {}.'.format(str(repo.head.commit.hexsha)))
 
+    rama = 
+
 import os
 from git import Repo
 repo_path = os.getenv(url_folder)
 repo = Repo(repo_path)
 if not repo.bare:
-    print('Repo at {} successfully loaded.'.format(repo_path))
     print_repository(repo)
+
+    # create list of commits then print some of them to stdout
     commits = list(repo.iter_commits('master'))[:COMMITS_TO_PRINT]
     for commit in commits:
         print_commit(commit)
